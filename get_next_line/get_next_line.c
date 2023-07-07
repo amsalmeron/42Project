4358b35d447be56e4bb1b7b813f64c189339baf4
Antonio Salmeron <mr.tonyboi@gmail.com>
2023-07-07 12:21:20 -0700

    get next line: draft

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -12,26 +12,102 @@
 
 #include "get_next_line.h"
 
+static char	*ft_read_line(int fd, char *buf, char *data_buffer)
+{
+	int	read_bytes;
+	char	*temp;
+
+	read_bytes = 1;
+	while (read_bytes != 0)
+	{
+		read_bytes = read(fd, buf, BUFFER_SIZE);
+		if (read_bytes == -1)
+			return (NULL);
+		else if (read_bytes == 0)
+			break ;
+		buf[read_bytes] = '\0';
+		if (data_buffer == NULL)
+			data_buffer = ft_strdup("");
+		temp = data_buffer;
+		data_buffer = ft_strjoin(temp, buf);
+		free(temp);
+		temp = NULL;
+		if (ft_strchr (buf, '\n'))
+			break;
+	}
+	return (data_buffer);
+}
+
+static char *ft_new_line(char *line)
+{
+	size_t	i;
+	char	*new_line;
+
+	i = 0;
+	while (line[i] != '\n' && line[i] != '\0')
+		i++;
+	if (line[i] == '\n' || line[i] == '\0')
+		return (NULL);
+	new_line = ft_substr(line, i + 1, ft_strlen(line) - i - 1);
+	if (*new_line == '\0')
+	{
+		free(new_line);
+		return (NULL);
+	}
+	line[i + 1] = '\0';
+	return (new_line);
+}
+
 char	*get_next_line(int fd)
 {
 	char		*line;
-	static char	*buf;
+	char		*buf;
+	static char	*data_buffer;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-
-
-
-	line = "hello, world";
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (buf == NULL)
+		return (NULL);
+	line = ft_read_line(fd, buf, data_buffer);
+	free(buf);
+	if (line == NULL)
+		return (NULL);
+	data_buffer = ft_new_line(line);
 	return (line);
 }
-/*
+
+#include <fcntl.h>
+#include <stdio.h>
+
+void test_get_next_line(int fd)
+{
+	char *line;
+	int line_number = 1;
+	/*
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("Line %d: %s\n", line_number, line);
+		free(line);
+		line_number++;
+	}
+	*/
+	line = get_next_line(fd);
+	printf("Line %d: %s\n", line_number, line);
+	
+}
+
 int main()
 {
-	int fd;
+	int fd = open("test.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Failed to open the file.\n");
+		return 1;
+	}
 
-	fd = open("test.txt", O_RDONLY);
-	
-	
-	return (0);
-}*/
+	test_get_next_line(fd);
+
+	close(fd);
+	return 0;
+}
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -13,3 +13,94 @@
 #include "get_next_line.h"
 
 // Functions here
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	str_len;
+	size_t	i;
+
+	str_len = ft_strlen(s);
+	if (start >= str_len)
+		return (ft_strdup(""));
+	if (str_len - start < len)
+		len = str_len - start;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (s[start] && i < len)
+		str[i++] = s[start++];
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s2;
+	size_t	i;
+	size_t	length;
+
+	length = ft_strlen(s1);
+	s2 = malloc(sizeof(char) * (length + 1));
+	if (!s2)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*joined;
+	int		length;
+	int		i;
+	int		j;
+
+	length = ft_strlen(s1) + ft_strlen(s2) + 1;
+	i = 0;
+	j = 0;
+	joined = malloc(sizeof(char) * length);
+	if (joined == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		joined[i] = s2[j];
+		i++;
+		j++;
+	}
+	joined[i] = '\0';
+	return (joined);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == (char)c)
+		return ((char *)s);
+	return (0);
+}
